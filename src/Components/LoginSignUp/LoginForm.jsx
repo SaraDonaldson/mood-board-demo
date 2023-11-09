7e9b9093a3316225c2f62e0714f8b9b6f6b55aec
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-09 18:25:54 +0000

    basic login and sign up page
    - component for login
    - component for sign up
    - redirected to dashboard

---
+++ src/Components/LoginSignUp/LoginForm.jsx
@@ -0,0 +1,68 @@
+
+import React, { useState } from "react";
+import { Navigate, redirect, useNavigate } from 'react-router-dom';
+
+
+
+function LoginForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); 
+    const navigate = useNavigate();
+
+    // function validateForm() {
+    //   return email.length > 0 && password.length > 0;
+  
+    // }
+  
+    function handleSubmit(event) {
+  
+    //   event.preventDefault();
+    return navigate("/dashboard");
+  
+    }
+  return (
+    <div className='login-form'>
+        <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+<div className="form-field">
+  <label>Email</label>
+  <input
+    autoFocus
+    type="email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+  />
+</div>
+<div className="form-field">
+  <label>Password</label>
+  <input
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+  />
+</div>
+
+
+<button className="form-button" type="submit" 
+// disabled={!validateForm()}
+>
+
+  Login
+
+</button>
+
+</form>
+    </div>
+  )
+}
+
+export default LoginForm
+
+
+
+
+
+   
+
+
