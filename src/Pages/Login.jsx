47abf9ceffd60228da9a856b6efa113dbca7c03d
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-09 18:28:10 +0000

    create login page
    - create login form
    - later: add functionality - right now, no local storage or backend

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import LoginForm from '../Components/LoginSignUp/LoginForm.jsx'
+import "./Login.css";
+
+function Login() {
+
+
+
+
+  return (
+    <div className='login-sign-up'>
+
+        <div className='form-container'>
+    <LoginForm/>
+        </div>
+
+    </div>
+  )
+}
+
+export default Login
