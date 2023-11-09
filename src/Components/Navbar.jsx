05eb1e7daa18dbb089219c431c27ec262f3585e0
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-09 18:27:28 +0000

    Style and add navbar
    - create basic dynamic navbar
    - create "logo" from css gradient with text
    - add color scheme to navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './navbar.css'
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className='logo-container'>
+        <div className="logo">
+          Creative Moods
+        </div>
+        </div>
+
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          ▲
+         </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">New Moodboard</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Dashboard</NavLink>
+            </li>
+            {/* <li>
+              <NavLink to="/blog">Blog</NavLink>
+            </li> */}
+            {/* <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
