dce23e33ff278d1d1a1501f1e7183128a76d9026
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-09 18:25:01 +0000

    add react router
    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import CreateMoodBoard from './CreateMoodBoard';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Login from './Pages/Login';
+import UserDashboard from './Pages/UserDashboard';
 
 function App() {
   return (
     <div className="App">
-      <CreateMoodBoard/>
+       <HashRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<CreateMoodBoard/> } />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
+    <Footer />
+</HashRouter>
+     
 
       </div>
   );
