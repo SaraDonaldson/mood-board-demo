49c3fbc5f8b912e5731a19bf9b5f2b31f20eb39a
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-11-10 18:04:57 +0000

    login form toggle with conditional css

--- src/Components/LoginSignUp/SignUpForm.jsx
+++ src/Components/LoginSignUp/SignUpForm.jsx
@@ -19,7 +19,7 @@ function SignUpForm() {
     }
   return (
     <div className='login-form'>
-       <h1>Create an Account</h1>
+
       <form onSubmit={handleSubmit}>
 <div className="form-field">
   <label>Email</label>
